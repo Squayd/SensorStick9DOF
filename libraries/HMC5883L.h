bd9e96476f59e92fd092cf9e986c612916968bf5
Squayd <carroll.william.c@gmail.com>
2016-08-29 12:47:47 -0700

    Begin magnetometer library
    Started on header for magnetometer library.

---
+++ libraries/HMC5883L.h
@@ -0,0 +1,23 @@
+/*
+ * Arduino library for interacting with Honeywell HMC5883L magnetometer
+ *
+ * Written as part of SensorStick9DOF library for the Sparkfun
+ * 9DOF sensor stick, should be applicable for any interaction with
+ * this particular IC.
+ *
+ * William Carroll
+ * https://github.com/squayd
+ * Begun 29 August 2016
+ * 
+ * Following initialization and use examples from Razor_AHRS Firmware 
+ * https://github.com/ptrbrtz/razor-9dof-ahrs
+ *
+ */
+ #ifndef _HMC5883L_H_USED_
+ #define _HMC5883L_H_USED_
+ 
+ #include <Wire.h>
+ 
+ 
+ 
+ #dendif
